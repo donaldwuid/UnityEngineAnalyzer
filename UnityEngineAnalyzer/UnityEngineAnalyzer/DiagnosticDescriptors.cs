199d6d42372eecaea813bd927b64257f3314008f
Vinny <vinny.dasilva@gmail.com>
2016-12-14 15:03:31 -0500

    Removing references to Resource classes
    Not currently supported by .Net Standard

--- UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticDescriptors.cs
+++ UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticDescriptors.cs
@@ -1,140 +1,134 @@
 ﻿using Microsoft.CodeAnalysis;
-using UnityEngineAnalyzer.AOT;
-using UnityEngineAnalyzer.CompareTag;
-using UnityEngineAnalyzer.Coroutines;
-using UnityEngineAnalyzer.EmptyMonoBehaviourMethods;
-using UnityEngineAnalyzer.FindMethodsInUpdate;
-using UnityEngineAnalyzer.ForEachInUpdate;
-using UnityEngineAnalyzer.IL2CPP;
-using UnityEngineAnalyzer.OnGUI;
-using UnityEngineAnalyzer.StringMethods;
 
 namespace UnityEngineAnalyzer
 {
     static class DiagnosticDescriptors
     {
+        //IMPORTANT! As part of the process of migrating to .Net Standard - Reference to resources were removed as
+        // Support for resource class generation is currently limited
+
+
         //NOTES: Naming of Descriptors are a bit inconsistant
         //NOTES: The Resource Reading code seems  repetative
 
         public static readonly DiagnosticDescriptor DoNotUseOnGUI = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseOnGUI,
-            title: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.Title), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.MessageFormat), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)),
+            title: "Usage of OnGUI is known to cause GC and/or performance issues.",
+            messageFormat: "'{0}' implements OnGUI. This is known to cause GC and/or performance issues.",
             category: DiagnosticCategories.GC,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.Description), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)));
+            description: "Do not use OnGUI in MonoBehaviours.");
 
         public static readonly DiagnosticDescriptor DoNotUseStringMethods = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseStringMethods,
-            title: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.Title), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.MessageFormat), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)),
+            title: "Do not use SendMessage, SendMessageUpwards or BroadcastMessage.",
+            messageFormat: "Use of SendMessage, SendMessageUpwards, BroadcastMessage, Invoke or InvokeRepeating leads to code that is hard to maintain. Consider using UnityEvent, C# event, delegates or a direct method call.",
             category: DiagnosticCategories.StringMethods,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.Description), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)));
+            description: "Use of SendMessage, SendMessageUpwards, BroadcastMessage, Invoke or InvokeRepeating leads to code that is hard to maintain. Consider using UnityEvent, C# event, delegates or a direct method call.");
 
         public static readonly DiagnosticDescriptor DoNotUseCoroutines = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseCoroutines,
-            title: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.Title), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.MessageFormat), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)),
+            title: "Use of coroutines is known to cause some allocations.",
+            messageFormat: "Use of coroutines is known to cause some allocations.",
             category: DiagnosticCategories.GC,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.Description), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)));
+            description: "Use of coroutines is known to cause some allocations.");
 
 
         public static readonly DiagnosticDescriptor EmptyMonoBehaviourMethod = new DiagnosticDescriptor(
             id: DiagnosticIDs.EmptyMonoBehaviourMethod,
-            title: new LocalizableResourceString(nameof(EmptyMonoBehaviourMethodsResources.Title), EmptyMonoBehaviourMethodsResources.ResourceManager, typeof(EmptyMonoBehaviourMethodsResources)),
-            messageFormat: new LocalizableResourceString(nameof(EmptyMonoBehaviourMethodsResources.MessageFormat), EmptyMonoBehaviourMethodsResources.ResourceManager, typeof(EmptyMonoBehaviourMethodsResources)),
+            title: "Remove empty MonoBehaviour methods.",
+            messageFormat: "'{0}' has an empty MonoBehaviour method '{1}'. Empty MonoBehaviour methods causes some overhead.",
             category: DiagnosticCategories.Miscellaneous,
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(EmptyMonoBehaviourMethodsResources.Description), EmptyMonoBehaviourMethodsResources.ResourceManager, typeof(EmptyMonoBehaviourMethodsResources)));
+            description: "Empty MonoBehaviour methods causes some overhead.");
 
         public static readonly DiagnosticDescriptor UseCompareTag = new DiagnosticDescriptor(
             id: DiagnosticIDs.UseCompareTag,
-            title: new LocalizableResourceString(nameof(UseCompareTagResources.Title), UseCompareTagResources.ResourceManager, typeof(UseCompareTagResources)),
-            messageFormat: new LocalizableResourceString(nameof(UseCompareTagResources.MessageFormat), UseCompareTagResources.ResourceManager, typeof(UseCompareTagResources)),
+            title: "Use CompareTag",
+            messageFormat: "Use CompareTag instead of using the Equals operator",
             category: DiagnosticCategories.GC,
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(UseCompareTagResources.Description), UseCompareTagResources.ResourceManager, typeof(UseCompareTagResources)));
+            description: "Comparing a Tag using Equals operator can cause unecessary memory allocation. Use the CompareTag function Instead.");
 
         public static readonly DiagnosticDescriptor DoNotUseFindMethodsInUpdate = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseFindMethodsInUpdate,
-            title: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.Title), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.MessageFormat), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)),
+            title: "Cache the result of Find or GetComponent in Start or Awake.",
+            messageFormat: "The method {0} is called from {1}.{2} which could cause performance problems. Cache the result from {0} in Start or Awake instead.",
             category: DiagnosticCategories.Performance,
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.Description), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)));
+            description: "Using Find or GetComponent in Update methods can impact performance. Cache the result on Start or Awake methods");
 
         public static readonly DiagnosticDescriptor DoNotUseFindMethodsInUpdateRecursive = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseFindMethodsInUpdate,
-            title: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.Title), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.MessageFormatRecursive), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)),
+            title: "Cache the result of Find or GetComponent in Start or Awake.",
+            messageFormat: "The method {0}.{1} calls {2} which eventually calls {3} which could impact performance. Cache the result from {3} in Start or Awake instead.",
             category: DiagnosticCategories.Performance,
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.Description), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)));
+            description: "Using Find or GetComponent in Update methods can impact performance. Cache the result on Start or Awake methods");
 
         public static readonly DiagnosticDescriptor DoNotUseRemoting = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseRemoting,
-            title: new LocalizableResourceString(nameof(DoNotUseRemotingResources.Title), DoNotUseRemotingResources.ResourceManager, typeof(DoNotUseRemotingResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseRemotingResources.MessageFormat), DoNotUseRemotingResources.ResourceManager, typeof(DoNotUseRemotingResources)),
+            title: "AOT Limitation: System.Runtime.Remoting is not supported",
+            messageFormat: "AOT Limitation: System.Runtime.Remoting is not supported",
             category: DiagnosticCategories.AOT,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseRemotingResources.Description), DoNotUseRemotingResources.ResourceManager, typeof(DoNotUseRemotingResources)));
+            description: "AOT Limitation: System.Runtime.Remoting is not supported");
 
         public static readonly DiagnosticDescriptor DoNotUseReflectionEmit = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseReflectionEmit,
-            title: new LocalizableResourceString(nameof(DoNotUseReflectionEmitResources.Title), DoNotUseReflectionEmitResources.ResourceManager, typeof(DoNotUseReflectionEmitResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseReflectionEmitResources.MessageFormat), DoNotUseReflectionEmitResources.ResourceManager, typeof(DoNotUseReflectionEmitResources)),
+            title: "AOT Limitation: System.Reflection.Emit is not supported.",
+            messageFormat: "AOT Limitation: System.Reflection.Emit is not supported.",
             category: DiagnosticCategories.AOT,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseReflectionEmitResources.Description), DoNotUseReflectionEmitResources.ResourceManager, typeof(DoNotUseReflectionEmitResources)));
+            description: "AOT Limitation: System.Reflection.Emit is not supported.");
 
         public static readonly DiagnosticDescriptor TypeGetType = new DiagnosticDescriptor(
             id: DiagnosticIDs.TypeGetType,
-            title: new LocalizableResourceString(nameof(TypeGetTypeResources.Title), TypeGetTypeResources.ResourceManager, typeof(TypeGetTypeResources)),
-            messageFormat: new LocalizableResourceString(nameof(TypeGetTypeResources.MessageFormat), TypeGetTypeResources.ResourceManager, typeof(TypeGetTypeResources)),
+            title: "AOT Limitation: Only works for looking up existing types.",
+            messageFormat: "AOT Limitation: Only works for looking up existing types.",
             category: DiagnosticCategories.AOT,
             defaultSeverity: DiagnosticSeverity.Info,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(TypeGetTypeResources.Description), TypeGetTypeResources.ResourceManager, typeof(TypeGetTypeResources)));
+            description: "AOT Limitation: Only works for looking up existing types.");
 
-        public static readonly DiagnosticDescriptor DoNotUseForEachInUpdate  = new DiagnosticDescriptor(
+        public static readonly DiagnosticDescriptor DoNotUseForEachInUpdate = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseForEachInUpdate,
-            title: new LocalizableResourceString(nameof(DoNotUseForEachInUpdateResources.Title), DoNotUseForEachInUpdateResources.ResourceManager, typeof(DoNotUseForEachInUpdateResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseForEachInUpdateResources.MessageFormat), DoNotUseForEachInUpdateResources.ResourceManager, typeof(DoNotUseForEachInUpdateResources)),
+            title: "Avoid using ForEach loops in Update methods",
+            messageFormat: "The method {0}.{1} is using a ForEach loop",
             category: DiagnosticCategories.Performance,
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseForEachInUpdateResources.Description), DoNotUseForEachInUpdateResources.ResourceManager, typeof(DoNotUseForEachInUpdateResources))
-        );
+            description: "Using ForEach in an Update could be slower and use more memory than a For loop");
 
         public static readonly DiagnosticDescriptor UnsealedDerivedClass = new DiagnosticDescriptor(
               id: DiagnosticIDs.UnsealedDerivedClass,
-              title: new LocalizableResourceString(nameof(UnsealedDerivedClassResources.Title), UnsealedDerivedClassResources.ResourceManager, typeof(UnsealedDerivedClassResources)),
-              messageFormat: new LocalizableResourceString(nameof(UnsealedDerivedClassResources.MessageFormat), UnsealedDerivedClassResources.ResourceManager, typeof(UnsealedDerivedClassResources)),
+              title: "Unsealed Derived Class",
+              messageFormat: "Method {0} in Derived Class {1} is not sealed. Sealing the method or class improves performance.",
               category: DiagnosticCategories.Performance,
               defaultSeverity: DiagnosticSeverity.Warning,
               isEnabledByDefault: true,
-              description: new LocalizableResourceString(nameof(UnsealedDerivedClassResources.Description), UnsealedDerivedClassResources.ResourceManager, typeof(UnsealedDerivedClassResources))
+              description: "Unsealed Methods in Derived classes can impact performance"
         );
 
         public static readonly DiagnosticDescriptor InvokeFunctionMissing = new DiagnosticDescriptor(
               id: DiagnosticIDs.InvokeFunctionMissing,
-              title: new LocalizableResourceString(nameof(InvokeFunctionMissingResources.Title), InvokeFunctionMissingResources.ResourceManager, typeof(InvokeFunctionMissingResources)),
-              messageFormat: new LocalizableResourceString(nameof(InvokeFunctionMissingResources.MessageFormat), InvokeFunctionMissingResources.ResourceManager, typeof(InvokeFunctionMissingResources)),
+              title: "The function being invoked does not exist",
+              messageFormat: "The function '{0}' is invoking the method '{1}' that does not exist",
               category: DiagnosticCategories.Performance,
               defaultSeverity: DiagnosticSeverity.Warning,
               isEnabledByDefault: true,
-              description: new LocalizableResourceString(nameof(InvokeFunctionMissingResources.Description), InvokeFunctionMissingResources.ResourceManager, typeof(InvokeFunctionMissingResources))
+              description: "Invoke Function is Missing"
             );
     }
 }
